3b164c0b25969c5ffdd6a755327ee6d204f2eb9e
Gary McWilliams <gary.mcwilliams@verint.com>
2018-03-21 08:06:36 +0000

    Initial commit: working --topcount in basic/wordcount

---
+++ basic/wordcount.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+from platform import _release_filename
+from fileinput import filename
+import re
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+"""Wordcount exercise
+Google's Python class
+
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+
+Use str.split() (no arguments) to split on all whitespace.
+
+Workflow: don't build the whole program at once. Get it to an intermediate
+milestone and print your data structure and sys.exit(0).
+When that's working, try for the next milestone.
+
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+
+"""
+
+import sys
+
+# +++your code here+++
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
+
+###
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+
+
+def extract_words(filename):
+    words = {}
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            line_words = line.split(' ')
+            for word in line_words:
+                words_re = re.search(r'\w+', word)
+                if words_re == None:
+                    continue
+                if words_re.group() == '':
+                    continue
+                word = words_re.group().lower()
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
+
+    return order_bag_of_words(words, True)
+
+
+def order_bag_of_words(bag_of_words, desc=False):
+    words = [(word, cnt) for word, cnt in bag_of_words.items()]
+    return sorted(words, key=lambda x: x[1], reverse=desc)
+
+
+def print_words(filename):
+    for word, count in extract_words(filename):
+        print('%-12s: %d' % (word, count))
+
+
+def print_top(filename):
+    word_counts = extract_words(filename)
+
+    for word_count in word_counts[:20]:
+        print('%-12s: %d' % (word_count[0], word_count[1]))
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
